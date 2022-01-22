11faadaa62a52930fce6f7328a9feb78100baa2e
Daniel M. Zimmerman <dmz@galois.com>
2022-01-21 21:30:02 -0800

    CGPSA v1.8 with Backscatter Reduction
    Updated CGPSA to v1.8, adding the new backscatter reduction feature and fixing some minor typographical errors.

---
+++ README.md
@@ -0,0 +1,6 @@
+# CGPSA - A Spam Filter for CommuniGate (R) Pro - Version 1.8
+## Copyright (C) 2002-2022 TFF Enterprises, Written by Daniel M. Zimmerman
+
+License information is located in [LICENSE.md](LICENSE.md), and usage and
+installation documentation is located in 
+[documentation.html](documentation.html).
--- makedist.sh
+++ makedist.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Creating cgpsa-$1.tgz"
 mkdir cgpsa-$1
-cp -p cgpsa cgpsa.conf CLI.pm README documentation.html style.css cgpsa-$1
+cp -p cgpsa cgpsa.conf CLI.pm README.md LICENSE.md documentation.html style.css cgpsa-$1
 tar cvfz /data/www/tff/content/cgpsa/releases/cgpsa-$1.tgz cgpsa-$1 
 echo "Updating documentation..."
 cp documentation.html /data/www/tff/content/cgpsa/index.html
